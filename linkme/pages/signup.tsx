05a32521c13553a1b1b899ccf4c6eed00bc28ae7
Bill Huynh <billhuynh@MacBook-Air-2.local>
2023-08-22 23:32:37 -0700

    pushing all files

--- linkme/pages/signup.tsx
+++ linkme/pages/signup.tsx
@@ -1,8 +1,27 @@
+import supabase from "@/utils/supabaseClient";
 import { useState } from "react";
 
-export default function Login() {
+export default function Signup() {
     const[email, setEmail] = useState<string | undefined>();
     const[password, setPassword] = useState<string | undefined>();
+
+    async function signUpWithEmail(e) {
+      e.preventDefault();
+        try {
+          console.log(email, password)
+          if (email && password) {
+          const response = await supabase.auth.signUp({
+            email: email, 
+            password: password,
+          });
+          console.log('response', response);
+
+          if (response.error) throw response.error;
+          const userId = response.data.user?.id;
+          console.log("User ID: ", userId);
+        }
+      } catch {}
+    }
     return (
         <>
           {/*
@@ -21,7 +40,7 @@ export default function Login() {
                 alt="LinkMe"
               />
               <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Register Your Account
               </h2>
             </div>
     
@@ -39,6 +58,7 @@ export default function Login() {
                       autoComplete="email"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      onChange={(e) => setEmail(e.target.value)}
                     />
                   </div>
                 </div>
@@ -48,11 +68,6 @@ export default function Login() {
                     <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                       Password
                     </label>
-                    <div className="text-sm">
-                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                        Forgot password?
-                      </a>
-                    </div>
                   </div>
                   <div className="mt-2">
                     <input
@@ -62,6 +77,7 @@ export default function Login() {
                       autoComplete="current-password"
                       required
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      onChange={(e) => setPassword(e.target.value)}
                     />
                   </div>
                 </div>
@@ -70,8 +86,9 @@ export default function Login() {
                   <button
                     type="submit"
                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={signUpWithEmail}
                   >
-                    Sign in
+                    Sign Up
                   </button>
                 </div>
               </form>
--- linkme/utils/supabaseClient.ts
+++ linkme/utils/supabaseClient.ts
@@ -1,3 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+const supebaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""; 
+
+const supabase = createClient(supabaseUrl, supebaseAnonKey,);
+
+export default supabase;
+
+//{ auth: { persistSession: false },} <-- in case I need it 
