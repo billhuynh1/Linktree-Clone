1b01c48a73133be2265f5c1a9e7d38313903dbbc
unknown <billhuynh012@gmail.com>
2023-09-05 05:18:00 -0700

    add link feature

--- linkme/pages/index.tsx
+++ linkme/pages/index.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
 import supabase from '@/utils/supabaseClient'
 
-
-
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
   const [userId, setUserId] = useState<string | undefined>();
@@ -23,25 +21,28 @@ export default function Home() {
 
   const addNewLink = async () => {
     try {
-    if (title && url && userId){
-      const { data, error } = await supabase.from("links").insert({
-        title: title,
-        url: url,
-        user_id: userId
-      })
-      if (error) throw error;
-      console.log("data: ", data)
+      if (title && url && userId){
+        const { data, error } = await supabase.from("links").insert({
+          title: title,
+          url: url,
+          user_id: userId
+        }).select();
+        if (error) throw error;
+        console.log("data: ", data)
+      }
+    } catch (error) {
+      console.log("error: ", error)
     }
-  } catch (error) {
-    console.log("error: ", error)
-  }
   };
 
   return (
-  <div className='flex flex-col w-full justify-center'>
+  <div className='flex flex-col items-center justify-center mt-5'>
     {isAuthenticated && (
        <>
-        <div className="mt-1 w-1/2">
+        <div className="mt-1">
+          <div className='block text-sm font-medium text-gray-700'>
+            Title
+          </div>
           <input
             id="title"
             name="title"
@@ -52,22 +53,25 @@ export default function Home() {
           />
         </div>
 
-        <div className="mt-1 w-1/2">
+        <div className="mt-1">
+          <div className='block text-sm font-medium text-gray-700'>
+            URL
+          </div>
           <input
             id="url"
             name="url"
             type="text"
-            placeholder="https://youtube.com"
+            placeholder="https://google.com"
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             onChange={(e) => setUrl(e.target.value)}
           />
         </div>
     <button 
       type="button"
-      className="flex w-24 mt-4 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={addNewLink }
+      className="flex mt-4 mb-4 justify-center rounded-md bg-indigo-600 px-4 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      onClick={addNewLink}
     >
-      Add link
+      Add New link
     </button>
     </>
     )}
